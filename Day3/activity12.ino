59e6870be2d32e7fc9dd28adf8cbad09e2c14539
MS <72651078+maddydevgits@users.noreply.github.com>
2025-02-21 10:13:12 +0530

    updating activity-12 to day3

---
+++ Day3/activity12.ino
@@ -0,0 +1,19 @@
+#include<WiFi.h>
+const char* ssid="Madhu's iPhone";
+const char* password="1234";
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid,password);
+  while(WiFi.status()!=WL_CONNECTED){
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("Connected to Internet");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
