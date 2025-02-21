3292cd984fe55a7bbb4ea74f97b827f0737a1537
MS <72651078+maddydevgits@users.noreply.github.com>
2025-02-21 12:44:11 +0530

    updating activity16

---
+++ Day3/activity16.ino
@@ -0,0 +1,33 @@
+#include<WiFi.h>
+#include<ThingSpeak.h>
+
+const char* ssid="Madhu's iPhone";
+const char* password="1234";
+
+int channelid=2849585;
+int led=2;
+
+WiFiClient client;
+
+void setup() {
+  pinMode(led,OUTPUT);
+  Serial.begin(9600);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid,password);
+  while(WiFi.status()!=WL_CONNECTED){
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("Connected to Internet");
+  ThingSpeak.begin(client);
+}
+
+void loop() {
+  int value=ThingSpeak.readIntField(channelid,1);
+  Serial.println(value);
+  if(value>0)
+    digitalWrite(led,1);
+  else
+    digitalWrite(led,0);
+  delay(5000);
+}
