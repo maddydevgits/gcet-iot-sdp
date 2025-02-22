8b0fded30a7ec2666a46660a448fa947c5d024f1
MS <72651078+maddydevgits@users.noreply.github.com>
2025-02-22 12:09:11 +0530

    updating activity19

--- Day4/activity19.ino
+++ Day4/activity19.ino
@@ -1,1 +1,33 @@
-// Log Humidity Data and Temperature Data on Gauge (Blynk App)
+// Activity-19: Control LED from Blynk App
+
+#define BLYNK_TEMPLATE_ID ""
+#define BLYNK_TEMPLATE_NAME ""
+#define BLYNK_AUTH_TOKEN ""
+#define BLYNK_PRINT Serial
+
+#include<WiFi.h>
+#include<BlynkSimpleEsp32.h>
+
+int led=2;
+
+BLYNK_WRITE(V2){
+  int state=param.asInt();
+  if(state==1)
+    digitalWrite(led,1);
+  else
+    digitalWrite(led,0);
+}
+
+const char* ssid="Mad";
+const char* password="123456";
+
+
+void setup() {
+  pinMode(led,OUTPUT);
+  Serial.begin(9600);
+  Blynk.begin(BLYNK_AUTH_TOKEN,ssid,password);
+}
+
+void loop() {
+  Blynk.run();
+}
